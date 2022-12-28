7e2a866730c2fe45ff34a2cd2114c433f6fabe7b
Davis <davislimzh1@gmail.com>
2022-12-29 02:51:51 +0800

    finished the click to draw

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-let color ="black"
+let color ="black";
+let click = false;
 //use DOMContentLoaded to run Js after html and css has loaded to not crash the site!
 document.addEventListener('DOMContentLoaded', function(){
     createContainer(16);
+    document.querySelector("body").addEventListener('click', function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+        }
+        let drawMessage = document.querySelector("#drawMessage");
+        if(click){
+            drawMessage.innerHTML = "You can now draw!"
+        }else{
+            drawMessage.innerHTML = "Click to draw!"
+        }
+    })
     let btnSelector = document.querySelector("#popup");
     btnSelector.addEventListener("click", function(){
         let size = getSize();
@@ -23,6 +35,7 @@ function createContainer(size){
     }
 }
 
+//get the size of the container
 function getSize(){
     let input = window.prompt("Enter your preferred size!")
     let message = document.querySelector("#message");
@@ -36,12 +49,15 @@ function getSize(){
     }
 }
 
+//Choose the color
 function colorDiv(){
-    if (color == "random" ){
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-    }
-    else{
-        this.style.backgroundColor = "black";
+    if(click){
+        if (color == "random" ){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }
+        else{
+            this.style.backgroundColor = "black";
+        }
     }
 }
 
@@ -49,6 +65,7 @@ function setColor(colorChoice){
     color = colorChoice;
 }
 
+//reset the container
 function resetContainer(){
     let divs = document.querySelectorAll('div')
     divs.forEach((div) => div.style.backgroundColor = 'white')
