f387e6e67bd68c34d95a6fbcf8d711f831fc9089
Davis <davislimzh1@gmail.com>
2022-12-29 02:41:26 +0800

    Working with the buttons

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+let color ="black"
 //use DOMContentLoaded to run Js after html and css has loaded to not crash the site!
 document.addEventListener('DOMContentLoaded', function(){
     createContainer(16);
-    console.log("hi")
+    let btnSelector = document.querySelector("#popup");
+    btnSelector.addEventListener("click", function(){
+        let size = getSize();
+        createContainer(size);
+    });
 })
 
 //create CSSgrid
@@ -13,9 +18,38 @@ function createContainer(size){
     let numDivs = size * size;
     for(i = 0; i<numDivs; i++){
         let div = document.createElement('div');
-        div.style.backgroundColor = 'yellow'
-        div.style.cssText = "border-color: black;"
+        div.addEventListener("mouseover", colorDiv)// why does using colordiv() generate error??
         container.appendChild(div)
-
     }
+}
+
+function getSize(){
+    let input = window.prompt("Enter your preferred size!")
+    let message = document.querySelector("#message");
+    if(input == "" || input == null){
+        message.innerHTML = "Please provide a number"
+    } else if(input<0 || input>100){
+        message.innerHTML = "Please input between 0 and 100"
+    } else{
+    message.innerHTML = "Now you play!";
+    return input;
+    }
+}
+
+function colorDiv(){
+    if (color == "random" ){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else{
+        this.style.backgroundColor = "black";
+    }
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+function resetContainer(){
+    let divs = document.querySelectorAll('div')
+    divs.forEach((div) => div.style.backgroundColor = 'white')
 }
