8a49010a018b0beb6358c43bf408e57b978737f0
Davis <davislimzh1@gmail.com>
2022-12-29 01:31:55 +0800

    Added divs according to size using CSS grid template

--- main.js
+++ main.js
@@ -1,31 +1,21 @@
-//set impt constants and variables
+//use DOMContentLoaded to run Js after html and css has loaded to not crash the site!
+document.addEventListener('DOMContentLoaded', function(){
+    createContainer(16);
+    console.log("hi")
+})
 
-const container = document.querySelector('#container');
-const rows = document.querySelector('gridRow');
-const cells = document.querySelector('cell');
+//create CSSgrid
+function createContainer(size){
+    let container = document.querySelector('#container');
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`; //CSS grid template
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+     
+    let numDivs = size * size;
+    for(i = 0; i<numDivs; i++){
+        let div = document.createElement('div');
+        div.style.backgroundColor = 'yellow'
+        div.style.cssText = "border-color: black;"
+        container.appendChild(div)
 
-//make default grid
-function defaultGrid(){
-    makeRows(16);
-    makeColumns(16);
-}
-
-//make rows
-function makeRows(x) {
-    for (r = 0; r < x; r++) {
-        let row = document.createElement('div');
-        container.appendChild(row).className = "gridRow";
     }
 }
-
-//make columns
-function makeColumns(y) {
-    for(i = 0; i<rows.length; i++) {
-        for(j = 0; j<y; j++){
-            let newCells = document.createElement('div');
-            container.appendChild(newCells).className = "cell";
-        }
-    }
-}
-
-defaultGrid();
